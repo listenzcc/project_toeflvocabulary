98383863e4859b4a1e1ebb87ae992e6a117cc47a
zcc <listenzcc@mail.bnu.edu.cn>
2019-11-28 15:06:44 +0800

    Functional update.
    Improve server.py to print page in two columns.

--- project_v2/server.py
+++ project_v2/server.py
@@ -1,10 +1,17 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib.parse
-import json
 import os
+import json
+import random
+import urllib.parse
+from pprint import pprint
+from random_word import random_word
+from checkout_online import explain_word
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 data = {'result': 'this is a test'}
 host = ('localhost', 8619)
+
+def wrap(label, content):
+    return '<{label}> {content} </{label}>'.format(label=label, content=content)
 
 class Resquest(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -12,13 +19,24 @@ class Resquest(BaseHTTPRequestHandler):
         get = urllib.parse.urlparse(self.path)
         print(get)
         word = get.query
+
+        Right_side_contents = []
         print(word)
+        if word == '[random]':
+            word = random_word()
+            print(word)
+        word_explains = explain_word(word)
+        pprint(word_explains)
+        for name in word_explains:
+            Right_side_contents.append(wrap('h2', name))
+            Right_side_contents.append(wrap('p', word_explains[name]))
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         with open(os.path.join('html_format', 'text.html')) as f:
             s = f.readlines()
-            html = ''.join(s).format(**{'query_word': word})
+            html = ''.join(s).format(query_word=word, Right_side_contents='\n'.join(Right_side_contents))
 
         self.wfile.write(html.encode()) # json.dumps(data).encode())
 
